83212c3be89ca98fb8ced2c3a99995f034288645
manavisharma14 <manavisharma14@gmail.com>
2025-08-26 16:17:30 -0500

    m1

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 export default function Navbar(){
     return (
         <nav className="flex justify-between px-10 pt-6">
-            <Link href='/' className="font-extrabold text-xl">Manavi Blog</Link>
+            <Link href='/' className="font-extrabold text-xl">Manavi Writes</Link>
             <div className="font-bold">
                 <Link href='/' className="mx-4 text-gray-600 hover:text-gray-900">Home</Link>
-                <Link href='/blog' className="mx-4 text-gray-600 hover:text-gray-900">Blog</Link>
+                {/* <Link href='/blog' className="mx-4 text-gray-600 hover:text-gray-900">Blog</Link> */}
             </div>
         </nav>
     )
